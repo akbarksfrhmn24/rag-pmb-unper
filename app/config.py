3ff09354b9325f2e58349a5ef8d7ce64d68a6f30
Muhammad Akbar Kasyfurrahman <akbar.kasyfurrahman24@gmail.com>
2025-06-16 18:19:35 +0700

    Security: Remove API keys from config and add .env.example template

--- app/config.py
+++ app/config.py
@@ -8,4 +8,4 @@ VECTOR_STORE_PATH = os.getenv("VECTOR_STORE_PATH", "./data/chroma_db")
 # You can set this via an environment variable or replace "your_openai_api_key_here" with your actual key.
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 
-GITHUB_API_KEY = os.getenv("GITHUB_API_KEY", "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")
+GITHUB_API_KEY = os.getenv("GITHUB_API_KEY", "")
